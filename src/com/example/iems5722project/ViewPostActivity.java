1a11408126f0da20c15b8daae1ed36fcbb16f070
ZhengHuahua <sven.zhenghh@gmail.com>
2015-04-23 00:54:47 +0800

    add missed file

---
+++ src/com/example/iems5722project/ViewPostActivity.java
@@ -0,0 +1,12 @@
+package com.example.iems5722project;
+
+import android.os.Bundle;
+
+public class ViewPostActivity extends BaseActivity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.post_main_text);
+		//this.getIntent().getExtras().getString("url");
+		
+	}
+}
